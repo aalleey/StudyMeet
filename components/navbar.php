6db34ebe1114bef2b2f10be7f83df049cf769e8a
Wajdan <kwaji4926@gmail.com>
2024-03-07 03:31:02 +0500

    minor change

--- components/navbar.php
+++ components/navbar.php
@@ -1,40 +1,40 @@
-    <nav>
-        <div class="nav-bar">
-            <i class='bx bx-menu sidebarOpen' ></i>
-            <span class="logo navLogo"><a href="../welcome.php">Study Meet</a></span>
+<nav>
+    <div class="nav-bar">
+        <i class='bx bx-menu sidebarOpen'></i>
+        <span class="logo navLogo"><a href="../welcome.php">Study Meet</a></span>
 
-            <div class="menu">
-                <div class="logo-toggle">
-                    <span class="logo"><a href=../"welcome.php">Sudy Meet</a></span>
-                    <i class='bx bx-x siderbarClose'></i>
-                </div>
-
-                <ul class="nav-links">
-                    <li><a href="./welcome.php">Home</a></li>
-                    <li><a href="./allposts.php">All Posts</a></li>
-                    <li><a href="./components/tutuors.php">All Teachers</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
+        <div class="menu">
+            <div class="logo-toggle">
+                <span class="logo"><a href="../components/welcome.php">Sudy Meet</a></span>
+                <i class='bx bx-x siderbarClose'></i>
             </div>
 
-            <div class="darkLight-searchBox">
-                <div class="dark-light">
-                    <i class='bx bx-moon moon'></i>
-                    <i class='bx bx-sun sun'></i>
-                </div>
+            <ul class="nav-links">
+                <li><a href="./welcome.php">Home</a></li>
+                <li><a href="./allposts.php">All Posts</a></li>
+                <li><a href="./components/tutuors.php">All Teachers</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
 
-                <div class="searchBox">
-                   <div class="searchToggle">
+        <div class="darkLight-searchBox">
+            <div class="dark-light">
+                <i class='bx bx-moon moon'></i>
+                <i class='bx bx-sun sun'></i>
+            </div>
+
+            <div class="searchBox">
+                <div class="searchToggle">
                     <i class='bx bx-x cancel'></i>
                     <i class='bx bx-search search'></i>
-                   </div>
+                </div>
 
-                    <div class="search-field">
-                        <input type="text" placeholder="Search...">
-                        <i class='bx bx-search'></i>
-                    </div>
+                <div class="search-field">
+                    <input type="text" placeholder="Search...">
+                    <i class='bx bx-search'></i>
                 </div>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
 <script src="./js/navbar.js"></script>
